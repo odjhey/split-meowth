0740ff03eb11322e0ad3b79a43ac99cb71cf140b
Odelon Jhey Pacalso <odjhey@gmail.com>
2023-12-27 13:27:51 +0900

    feat: add expense

--- modules/core/src/surface/index.ts
+++ modules/core/src/surface/index.ts
@@ -1,7 +1,35 @@
+import { types } from "mobx-state-tree";
+
+type AddExpenseInput = {
+  name: string;
+  total: number;
+  // TODO currency: string;
+};
+
+const Expense = types.model({
+  name: types.string,
+  total: types.number,
+  // TODO currency: types.string,
+});
+const Ui = types
+  .model({
+    expenses: types.array(Expense),
+  })
+  .views((self) => ({}))
+  .actions((self) => ({
+    addExpense(input: AddExpenseInput) {
+      self.expenses.push(input);
+    },
+  }));
+
 const init = () => {
+  const ui = Ui.create({});
+
   return {
     ui: {
       hello: () => "world",
+      expenses: () => ui.expenses,
+      addExpense: (input: AddExpenseInput) => ui.addExpense(input),
     },
   } as const;
 };
--- modules/core/tests/ui/normie-user/normie-user.operations.tests.ts
+++ modules/core/tests/ui/normie-user/normie-user.operations.tests.ts
@@ -1,11 +1,67 @@
 import t from "tap";
 import { init } from "../../../src";
 
-const getUi = () => init().ui;
+const getStore = () => init();
 
 t.test("bigbang", async (t) => {
   t.plan(1);
-  const ui = getUi();
+  const ui = getStore().ui;
 
   t.equal(ui.hello(), "world");
+});
+
+// TODO: future,
+//          paidByBreakdown: {
+//            joe: 100,
+//          },
+
+// item name, total price, peeps involved, paid by
+//   paid by breakdown
+//   peeps involved breakdown
+
+// example schema below
+// {
+//   name: "dinner",
+//   total: 100,
+//   peeps: ["joe", "jane"],
+//   paidBy: "joe",
+//   paidByBreakdown: {
+//     joe: 100,
+//   },
+//   sharingBreakdown: {
+//     joe: 50,
+//     jane: 50,
+//   },
+// }
+
+// report example below per user, show what they owe and what they are owed, reference the items
+// {
+//   joe: {
+//     owes: {
+//       jane: 50,
+//       items: ["dinner"],
+//     },
+//     owed: {
+//       jane: 50,
+//     },
+//   },
+
+// feature list based of above
+//   add item
+//   add peep
+//   add payment
+//   add payment breakdown
+//   add peep breakdown
+
+t.test("add entry, joe and jane went to dinner 2x", async (t) => {
+  t.plan(1);
+  const store = getStore();
+  const ui = store.ui;
+  ui.addExpense({ name: "dinner", total: 100 });
+  ui.addExpense({ name: "dinner 2", total: 90 });
+
+  t.same(ui.expenses(), [
+    { name: "dinner", total: 100 },
+    { name: "dinner 2", total: 90 },
+  ]);
 });
